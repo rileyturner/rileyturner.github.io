3baa0b58a8b829dfde254d92daca23940adc2c4e
Viydal <viydal@gmail.com>
2025-07-23 21:31:07 +0930

    updated

--- index.js
+++ index.js
@@ -12,6 +12,29 @@ document.addEventListener("DOMContentLoaded", () => {
   allElements.forEach((element) => observor.observe(element));
 })
 
+document.addEventListener("DOMContentLoaded", () => {
+    let timeline = document.querySelector(".timeline");
+    let containers = document.querySelectorAll(".timeline .container");
+
+    const observor = new IntersectionObserver((entries, observor) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          containers.forEach(container => {
+            console.log("viewing")
+            container.classList.add("inView");
+          });
+          observor.unobserve(entry.target); // Stop observing after first trigger
+        }
+      });
+    });
+
+    IntersectionObserver.thresholds
+
+    if (timeline) {
+      observor.observe(timeline);
+    }
+  });
+
 document.addEventListener('scroll', function () {
   let scrollPosition = window.scrollY || window.pageYOffset;
 
